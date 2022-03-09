6c76253e2f27bf016d8a94b372b594c8a0d11a04
Jason VenHuizen <jason@sharptree.io>
2022-03-09 14:45:48 -0800

    More pipeline fixes

--- deploy/deploy-application.py
+++ deploy/deploy-application.py
@@ -8,9 +8,7 @@ def updateModuleApp():
 
     moduleList = "+WebSphere:cell=" + cellName + ",server=" + serverName + \
         "+WebSphere:cell=" + cellName + ",node=" + nodeName + ",server=" + webserverName
-
     print "Updating module mapping with " + moduleList
-
     AdminApp.edit(appName, ['-MapModulesToServers',[['.*', '.*', moduleList]]])
     saveAndSync()
 
@@ -19,8 +17,10 @@ apps = AdminApp.list("WebSphere:cell="+cellName +",node="+ nodeName + ",server="
 if appName in apps:
    print "Updating the application."
    AdminApp.update(appName, 'app', ['-operation', 'update', '-contents', filePath,],)
+   saveAndSync()
 else:
    print "Installing the application."
    AdminApp.install(filePath,['-distributeApp','-usedefaultbindings','-cell',cellName,'-node',nodeName, '-server',serverName,'-appname', appName, '-MapWebModToVH',[['.*','.*',virtualHost]]])
+   saveAndSync()
 
 updateModuleApp()
