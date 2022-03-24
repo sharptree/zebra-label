90db9524b8f79ccb517c9a5fb301504a3292acea
Jason VenHuizen <jason@sharptree.io>
2022-03-24 13:50:04 -0700

    Add support for local Maximo library dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.kotlin.dsl.support.zipTo
-
 plugins {
     java
     distribution
@@ -24,6 +22,20 @@ tasks.compileJava {
 
 repositories {
     mavenCentral()
+
+    /**
+     * https://archiva.sharptree.dev is Sharptree's Maven repository that hosts Maximo dependencies in a private repository.
+     *
+     *  A username and password are required access the private repository.  The username and password are configured in the
+     *  [USER_HOME]/.gradle/gradle.properties file.
+     * archivaUserName=[username]
+     * archivaPassword=[password]
+     *
+     * If not a member of the Sharptree development team, either update this with your own repository or remove and uncomment the
+     * appropriate dependencies for local library dependencies that can be placed in the lib folder.
+     *
+     * For information on setting up your own Archiva server see https://archiva.apache.org/
+     */
     maven {
         name = "sharptree"
         url = uri("https://archiva.sharptree.dev/repository/private")
@@ -188,11 +200,35 @@ tasks.jar {
 
 dependencies {
 
+    /*
+     * Library used for validating host names and IP addresses.
+     */
     implementation("com.google.guava:guava:31.0.1-jre")
 
+    /*
+     * The javax.servlet-api is required to compile DataBean classes, but is otherwise provided by WebSphere / WebLogic.
+     */
     compileOnly("javax.servlet:javax.servlet-api:4.0.1")
+
+    /**
+     * Maximo's libraries needed for compiling the application.
+     *
+     * asset-management - the businessobjects.jar
+     * webclient - classes from the maximouiweb/WEB-INF/classes folder
+     * tools - classes from the [SMP_HOME]/maximo/tools/maximo/classes folder
+     *
+     * These classes jars are proprietary to IBM and hosted on Sharptree's private Archiva server.
+     *
+     * If you are not a Sharptree developer, but have access to a Maximo instance you can zip the required into jar files and
+     * places them in the libs directory on this project.  The comment the non-local dependencies.
+     */
+
+    compileOnly(fileTree( "libs", { listOf("*.jar") } ))
+
+    /*
+     * Comment out or remove the following lines if using local Maximo jar dependencies.
+     */
     compileOnly("com.ibm.maximo:asset-management:7.6.1.2")
     compileOnly("com.ibm.maximo:webclient:7.6.1.2")
     compileOnly("com.ibm.maximo:tools:7.6.0.0")
-
 }
